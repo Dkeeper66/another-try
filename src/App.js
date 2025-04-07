1a4497211288361cd655227061d4d9852997e8cc
Fedor <voronin1904@gmail.com>
2025-04-07 16:53:46 +0300

    first iteration of test code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import React from 'react'
 
 
 
-
+const Profile = ()=>{
+  return(
+    <img
+    src='https://i.imgur.com/MK3eW3As.jpg'
+    alt='nu hz kto'
+    />
+  )
+}
 
 export default function App(){
   
   return(
     <>
-    <div>hello world</div>
+    <div>look at them</div>
+    <Profile />
+    <Profile />
+    <Profile />
     </>
   )
 }
