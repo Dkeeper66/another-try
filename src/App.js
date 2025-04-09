bd35e280cb8d57bc12fe88d97c36253390de1197
Fedor <voronin1904@gmail.com>
2025-04-09 18:04:43 +0300

    tryed pulling props in components and ifelse ways of displaying

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react'
 
 
 
-const Profile = ()=>{
+const Profile = (props)=>{
+  let isTrue = props.isTrue
   return(
+    <>
     <img
     src='https://i.imgur.com/MK3eW3As.jpg'
     alt='nu hz kto'
     />
+    <h1>O kak {isTrue && 'vot tak'} </h1>
+    </>
   )
 }
 
@@ -17,9 +21,9 @@ export default function App(){
   return(
     <>
     <div>look at them</div>
-    <Profile />
-    <Profile />
-    <Profile />
+    <Profile isTrue = {true}/>
+    <Profile isTrue = {false}/>
+    <Profile isTrue = {true}/>
     </>
   )
 }
